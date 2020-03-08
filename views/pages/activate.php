2b7462449731c523ffa87d2f00be29639d52a13d
Andjela Perlas <38106481+Andjela-Perlas@users.noreply.github.com>
2020-03-08 17:50:44 +0100

    Update activate.php

--- views/pages/activate.php
+++ views/pages/activate.php
@@ -15,6 +15,6 @@ if(isset($_GET['active'])) {
 
 <div class="container my-5">
 	<div class="jumbotron mt-4 bg-transparent">
-		<h1 class="text-center font-weight-light">Uspešno ste verifikovali Vaš nalog! Da bi ste mogli da koristite usluge sajta, morate se prijaviti!</h1>		
+		<h1 class="text-center font-weight-light">Uspešno ste verifikovali Vaš nalog! Da biste mogli da koristite usluge sajta, morate se prijaviti!</h1>		
 	</div>
 </div>
